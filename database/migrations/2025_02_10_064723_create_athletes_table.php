ef7da4311407ea05e0d1476cbce93ede57c98032
Yusufnova06 <yusufnovandra377@gmail.com>
2025-02-10 14:04:17 +0700

    feat: table atlet

---
+++ database/migrations/2025_02_10_064723_create_athletes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('athletes', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->enum('jenis_kelamin', ['LAKI_LAKI', 'PEREMPUAN']);
+            $table->unsignedBigInteger('nik');
+            $table->unsignedBigInteger('no_kk');
+            $table->uuid('kontingen_id');
+            $table->foreign('kontingen_id')->references('id')->on('kontingen')->onDelete('cascade');
+            $table->date('tanggal_lahir');
+            $table->string('tempat_lahir');
+            $table->decimal('berat_badan', 3, 2);
+            $table->decimal('tinggi_badan', 3, 2);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('athletes');
+    }
+};
