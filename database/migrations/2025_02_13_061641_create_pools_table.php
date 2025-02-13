7459d643319526b8493cc1d1aa2962f66111da7f
Yusufnova06 <yusufnovandra377@gmail.com>
2025-02-13 13:18:13 +0700

    feat: table pools

---
+++ database/migrations/2025_02_13_061641_create_pools_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pools', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('kategori_tanding_id')->constrained('kategori_tanding')->onDelete('cascade');
+            $table->foreignUuid('event_id')->constrained('events')->onDelete('cascade');
+            $table->integer('max_peserta')->default(4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pools');
+    }
+};
