50cdcc6e32b88c42ee760aa4613ed1462250182a
Yusufnova06 <yusufnovandra377@gmail.com>
2025-02-13 18:55:06 +0700

    feat: HasUuid for auto generate id's column

--- app/Models/DecisionTreeRules.php
+++ app/Models/DecisionTreeRules.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DecisionTreeRules extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuid;
 }
+ 
