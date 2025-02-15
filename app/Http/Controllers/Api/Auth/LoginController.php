96cfeac3e7380fcae2aa98ffb953e5416ad8fb8e
Yusufnova06 <yusufnovandra377@gmail.com>
2025-02-15 15:42:25 +0700

    feat: auth system register, login, auth login, logout

---
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Kontingen;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Spatie\RouteDiscovery\Attributes\Route;
+
+class LoginController extends Controller
+{
+    #[Route(method: 'POST')]
+    public function __invoke(Request $request): JsonResponse
+    {
+        $credentials = $this->getCredentials($request);
+
+        if (Auth::attempt($credentials)) {
+            /** @var Kontingen $user */
+            $user = Auth::user();
+
+            return response()->json(
+                [
+                    'status' => 'success',
+                    'message' => 'Login successfully.',
+                    'access_token' => $user->createToken('MyApp')->plainTextToken,
+                    'token_type' => 'Bearer',
+                    'expires_in' => 3600,
+                ],
+                200
+            );
+        } else {
+            return response()->json(
+                [
+                    'status' => 'invalid_grant',
+                    'message' => 'Invalid User credentials',
+                ],
+                401
+            );
+        }
+    }
+
+    private function getCredentials(Request $request): array
+    {
+        $fieldType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'no_hp_penanggung_jawab';
+
+        Log::info($fieldType);
+        return [
+            $fieldType => $request->username,
+            'password' => $request->password,
+            'deleted_at' => null,
+        ];
+
+    }
+}
