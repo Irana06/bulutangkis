84c69d0c0612316671b5e4846e2ca701e7daa93b
Yusufnova06 <yusufnovandra377@gmail.com>
2025-03-14 14:32:26 +0700

    fix: delete

--- resources/js/Pages/Src/Peserta/ListPeserta.jsx
+++ resources/js/Pages/Src/Peserta/ListPeserta.jsx
@@ -49,7 +49,10 @@ export default function ListPeserta() {
             label: "Avatar",
             render: (value, item) => (
                 <img
-                    src={item.foto_profile_url || `https://ui-avatars.com/api/?name=${item.name}`}
+                    src={
+                        item.foto_profile_url ||
+                        `https://ui-avatars.com/api/?name=${item.name}`
+                    }
                     alt={item.name}
                     className="w-10 h-10 rounded-full"
                 />
@@ -66,14 +69,37 @@ export default function ListPeserta() {
             key: "aksi",
             label: "Aksi",
             render: (value, item, index) => (
-                <div className={`${dropdownOpenIndex === index ? "flex flex-col" : ""} text-left  whitespace-nowrap`}>
-                    <Link href={`/peserta/${item.id}/detail`} className={`py-2 leading-none px-3 font-medium text-green-600 bg-green-500/20 hover:text-green-500 duration-150 hover:bg-gray-50 rounded-lg ${dropdownOpenIndex === index ? "" : "ml-2"} `}>
+                <div
+                    className={`${
+                        dropdownOpenIndex === index ? "flex flex-col" : ""
+                    } text-left  whitespace-nowrap`}
+                >
+                    <Link
+                        href={`/peserta/${item.id}/detail`}
+                        className={`py-2 leading-none px-3 font-medium text-green-600 bg-green-500/20 hover:text-green-500 duration-150 hover:bg-gray-50 rounded-lg ${
+                            dropdownOpenIndex === index ? "" : "ml-2"
+                        } `}
+                    >
                         Detail
                     </Link>
-                    <Link href={`/peserta/${item.id}/edit`} className={`py-2 leading-none mt-2 px-3 font-medium text-indigo-600 bg-indigo-500/20 hover:text-indigo-500 duration-150 hover:bg-gray-50 rounded-lg ${dropdownOpenIndex === index ? "" : "ml-2"} `}>
+                    <Link
+                        href={`/peserta/${item.id}/edit`}
+                        className={`py-2 leading-none mt-2 px-3 font-medium text-indigo-600 bg-indigo-500/20 hover:text-indigo-500 duration-150 hover:bg-gray-50 rounded-lg ${
+                            dropdownOpenIndex === index ? "" : "ml-2"
+                        } `}
+                    >
                         Edit
                     </Link>
-                    <Link onClick={() => handleDelete(item.id)} className={`py-2 leading-none mt-2 px-3 font-medium text-red-600 bg-red-500/20 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg ${dropdownOpenIndex === index ? "" : "ml-2"} `}>
+                    <Link
+                        href="#"
+                        onClick={(e) => {
+                            e.preventDefault(); // Mencegah fetch otomatis
+                            handleDelete(item.id);
+                        }}
+                        className={`py-2 leading-none mt-2 px-3 font-medium text-red-600 bg-red-500/20 hover:text-red-500 duration-150 hover:bg-gray-50 rounded-lg ${
+                            dropdownOpenIndex === index ? "" : "ml-2"
+                        } `}
+                    >
                         Delete
                     </Link>
                 </div>
