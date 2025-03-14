7e3743a7d08ba56efd20f839e3c958daeee309ce
Yusufnova06 <yusufnovandra377@gmail.com>
2025-03-14 14:14:09 +0700

    fix: detail tanding

--- resources/js/Pages/Src/Tanding/DetailTanding.jsx
+++ resources/js/Pages/Src/Tanding/DetailTanding.jsx
@@ -6,6 +6,7 @@ import BackButton from "@/Components/Buttons/BackButton";
 
 export default function DetailTanding() {
     const { tanding } = usePage().props;
+    console.log(tanding);
 
     // Fungsi untuk memformat string kategori tanding
     const formatJenisTanding = (jenis) => {
@@ -19,60 +20,173 @@ export default function DetailTanding() {
     return (
         <div className="max-w-3xl mx-auto p-2 text-gray-800">
             {/* Foto Profil */}
-            <div className="mb-10">
-                <img
-                    src={
-                        tanding.atlet?.foto_profile_url ??
-                        `https://ui-avatars.com/api/?name=${tanding.atlet?.name}`
-                    }
-                    alt="Foto Profil"
-                    className="w-32 h-32 object-cover rounded-full shadow-lg"
-                />
-            </div>
+            {tanding.atlet ? (
+                <div className="mb-10">
+                    <img
+                        src={
+                            tanding.atlet?.foto_profile_url ??
+                            `https://ui-avatars.com/api/?name=${tanding.atlet?.name}`
+                        }
+                        alt="Foto Profil"
+                        className="w-32 h-32 object-cover rounded-full shadow-lg"
+                    />
+                </div>
+            ) : null}
 
-            {/* Informasi Peserta */}
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Field label="Nama" value={tanding.atlet?.name} />
-                <Field
-                    label="Jenis Kelamin"
-                    value={
-                        tanding.atlet?.jenis_kelamin === "LAKI_LAKI"
-                            ? "Laki-Laki"
-                            : "Perempuan"
-                    }
-                />
-                <Field label="Umur" value={`${tanding.atlet?.umur} Tahun`} />
-                <Field label="Kontingen" value={tanding.kontingen?.name} />
-                <Field
-                    label="Tanggal Lahir"
-                    value={moment(tanding.atlet?.tanggal_lahir).format(
-                        "D MMM YYYY"
-                    )}
-                />
-                <Field
-                    label="Tempat Lahir"
-                    value={tanding.atlet?.tempat_lahir}
-                />
-                <Field
-                    label="Jenis Tanding"
-                    value={formatJenisTanding(tanding.kategori_tanding?.jenis)}
-                />
-                <Field
-                    label="Tingkat Tanding"
-                    value={tanding.kategori_tanding?.tingkat}
-                />
-                <Field
-                    label="Kelompok Tanding"
-                    value={tanding.kategori_tanding?.kelompok_umur}
-                />
-                <Field
-                    label="Juara Ke"
-                    value={tanding.juara ? `Juara ${tanding.juara}` : "Belum ada rekaman"}
-                    className={`${
-                        tanding.juara ? "" : "text-yellow-600 italic"
-                    }`}
-                />
-            </div>
+            {/* Informasi Peserta & Tim */}
+            {tanding.atlet ? (
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <>
+                        <Field label="Nama" value={tanding.atlet?.name} />
+                        <Field
+                            label="Jenis Kelamin"
+                            value={
+                                tanding.atlet?.jenis_kelamin === "LAKI_LAKI"
+                                    ? "Laki-Laki"
+                                    : "Perempuan"
+                            }
+                        />
+                        <Field
+                            label="Umur"
+                            value={`${tanding.atlet?.umur} Tahun`}
+                        />
+                        <Field
+                            label="Kontingen"
+                            value={tanding.kontingen?.name}
+                        />
+                        <Field
+                            label="Tanggal Lahir"
+                            value={moment(tanding.atlet?.tanggal_lahir).format(
+                                "D MMM YYYY"
+                            )}
+                        />
+                        <Field
+                            label="Tempat Lahir"
+                            value={tanding.atlet?.tempat_lahir}
+                        />
+                        <Field
+                            label="Jenis Tanding"
+                            value={formatJenisTanding(
+                                tanding.kategori_tanding?.jenis
+                            )}
+                        />
+                        <Field
+                            label="Tingkat Tanding"
+                            value={tanding.kategori_tanding?.tingkat}
+                        />
+                        <Field
+                            label="Kelompok Tanding"
+                            value={tanding.kategori_tanding?.kelompok_umur}
+                        />
+                        <Field
+                            label="Juara Ke"
+                            value={
+                                tanding.juara
+                                    ? `Juara ${tanding.juara}`
+                                    : "Belum ada rekaman"
+                            }
+                            className={`${
+                                tanding.juara ? "" : "text-yellow-600 italic"
+                            }`}
+                        />
+                    </>
+                </div>
+            ) : (
+                <>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <Field label="Nama Tim" value={tanding.tim?.nama_tim} />
+                        <Field
+                            label="Kontingen"
+                            value={tanding.kontingen?.name}
+                        />
+                        <Field
+                            label="Jenis Tanding"
+                            value={formatJenisTanding(
+                                tanding.kategori_tanding?.jenis
+                            )}
+                        />
+                        <Field
+                            label="Tingkat Tanding"
+                            value={tanding.kategori_tanding?.tingkat}
+                        />
+                        <Field
+                            label="Kelompok Tanding"
+                            value={tanding.kategori_tanding?.kelompok_umur}
+                        />
+                        <Field
+                            label="Juara Ke"
+                            value={
+                                tanding.juara
+                                    ? `Juara ${tanding.juara}`
+                                    : "Belum ada rekaman"
+                            }
+                            className={`${
+                                tanding.juara ? "" : "text-yellow-600 italic"
+                            }`}
+                        />
+                    </div>
+
+                    <hr className="my-3 mt-10 mb-10" />
+
+                    <h1 className="font-semibold text-xl mb-5">Peserta 1:</h1>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <Field label="Nama" value={tanding.tim?.atlet_1.name} />
+                        <Field
+                            label="Jenis Kelamin"
+                            value={
+                                tanding.tim?.atlet_1.jenis_kelamin ===
+                                "LAKI_LAKI"
+                                    ? "Laki-Laki"
+                                    : "Perempuan"
+                            }
+                        />
+                        <Field
+                            label="Umur"
+                            value={`${tanding.tim?.atlet_1.umur} Tahun`}
+                        />
+                        <Field
+                            label="Tanggal Lahir"
+                            value={moment(
+                                tanding.tim?.atlet_1.tanggal_lahir
+                            ).format("D MMM YYYY")}
+                        />
+                        <Field
+                            label="Tempat Lahir"
+                            value={tanding.tim?.atlet_1.tempat_lahir}
+                        />
+                    </div>
+
+                    <hr className="my-3 mt-5 mb-10" />
+
+                    <h1 className="font-semibold text-xl mb-5">Peserta 1:</h1>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <Field label="Nama" value={tanding.tim?.atlet_2.name} />
+                        <Field
+                            label="Jenis Kelamin"
+                            value={
+                                tanding.tim?.atlet_2.jenis_kelamin ===
+                                "LAKI_LAKI"
+                                    ? "Laki-Laki"
+                                    : "Perempuan"
+                            }
+                        />
+                        <Field
+                            label="Umur"
+                            value={`${tanding.tim?.atlet_2.umur} Tahun`}
+                        />
+                        <Field
+                            label="Tanggal Lahir"
+                            value={moment(
+                                tanding.tim?.atlet_2.tanggal_lahir
+                            ).format("D MMM YYYY")}
+                        />
+                        <Field
+                            label="Tempat Lahir"
+                            value={tanding.tim?.atlet_2.tempat_lahir}
+                        />
+                    </div>
+                </>
+            )}
             {/* Button Kembali */}
             <div className="mt-10">
                 <BackButton />
